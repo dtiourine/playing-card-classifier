16bd2bca3cf01aecafa7626b1ea27ce24056b8fb
DTiourine <109561478+DTiourine@users.noreply.github.com>
2024-06-04 15:41:35 -0400

    Works on FastAPI backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,22 +1,8 @@
-from fastapi import FastAPI, File, UploadFile
-from fastapi.middleware.cors import CORSMiddleware
-from backend.model.predict import make_prediction
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:8000",  # URL of the frontend
-    "http://localhost:5000",  # Adjust to actual frontend URL during deployment
-]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-@app.post("/predict/")
-async def predict(file: UploadFile = File(...)):
-    result = await make_prediction(file)
-    return {"prediction": result}
-
+@app.post("/test")
+async def test():
+    return JSONResponse(content={"message": "Test endpoint"})
--- backend/model/predict.py
+++ backend/model/predict.py
@@ -1,2 +1,27 @@
-def make_prediction():
-    pass
+from fastapi import UploadFile
+from PIL import Image
+import torch
+from torchvision import transforms
+import io
+from backend.util.dataloader import get_data_loaders
+import json
+
+async def make_prediction(model, file: UploadFile, device):
+
+    transform = transforms.Compose([ transforms.Resize((128, 128)), transforms.ToTensor(),])
+    with open('saved_model/target_to_class.json', 'r') as file:
+        target_to_class = json.load(file)
+
+    image_data = await file.read()
+    image = Image.open(io.BytesIO(image_data))
+    image_tensor = transform(image).unsqueeze(0)
+    image_tensor = image_tensor.to(device)
+
+    model.eval()
+    with torch.no_grad():
+        outputs = model(image_tensor)
+        _, predicted = torch.max(outputs, 1)
+
+    label = target_to_class(predicted.item())
+
+    return label.item()
